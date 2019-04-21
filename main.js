3a2004e407d40427c93b6e6d2d9ffb0f94b588cc
Joe Gibson <joe.gibby@hotmail.co.uk>
2019-04-21 11:44:14 +0100

    Should work with Heroku now

--- main.js
+++ main.js
@@ -1,9 +1,26 @@
 const Discord = require("discord.js");
-const config = require("./config.json");
+
+function getConfig() {
+	try {
+		const config = require("./config.json");
+		const token = config.token;
+		const prefix = config.prefix;
+		console.log("Starting using locally stored value for token.");
+		return {"token": token, "prefix": prefix}
+	}
+	catch(error) {
+		const token = process.env.TOKEN;
+		const prefix = ".";
+		console.log("Starting using token stored on Heroku");
+		return {"token": token, "prefix": prefix}
+	}
+}
+
+const config = getConfig()
 const { AkairoClient } = require("discord-akairo");
 const client = new AkairoClient({
     ownerID: "286601488703291395",
-    prefix: config.prefix,
+    prefix: config["prefix"],
     commandDirectory: "./commands/",
     inhibitorDirectory: "./inhibitors/",
     listenerDirectory: "./listeners/",
@@ -12,4 +29,4 @@ const client = new AkairoClient({
 	disableEveryone: true
 });
 
-client.login(config.token);//process.env.TOKEN
+client.login(config["token"]);
